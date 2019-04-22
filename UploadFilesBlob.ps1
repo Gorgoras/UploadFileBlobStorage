b9dd09cb0dc7f4617565ac32307532adf655abfa
Martin Zurita <martinzurita1@gmail.com>
2019-04-21 22:26:37 -0300

    Add files via upload

---
+++ UploadFilesBlob.ps1
@@ -0,0 +1,14 @@
+﻿#Datos de blob storage
+$StorageAccountName = "AccName" 
+$StorageAccountKey = "AccKey"
+$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$ContainerName = "container-name"
+$target = "C:\Folder\To\Files\"
+
+ #subimos los archivos al blob storage y lo eliminamos del directorio local
+$archivosDescomprimidos = Get-ChildItem -Path $target
+$archivosDescomprimidos | ForEach-Object($_){
+    $BlobNameBackup = "gdm-rawdata\$($_.Name)"
+    Set-AzureStorageBlobContent -File "$($target)\$($_.Name)" -Container $ContainerName -Blob $BlobNameBackup `
+                                -Context $ctx -Force
+}
